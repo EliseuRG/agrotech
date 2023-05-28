35ed3bcff0ee3bbdd4f44474f785c9479a7b7ec2
Eliseu Rodrigues <eliseu.rguimaraes@gmail.com>
2023-05-28 16:33:27 -0300

    Inserir Registros de Funcionario

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,4 +14,33 @@ class PostController extends Controller
         //dd($posts);
         return view('admin.posts.index', compact('posts'));
     }
+
+    public function create()
+    {
+        return view('admin.posts.create');
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->all(); // Obter todos os dados do request
+        //Tratar Telefone
+        $data['telefone_func'] = preg_replace('/[^0-9]/', '', $data['telefone_func']);
+        
+        // Remover acentuação
+        $data['nome_func'] = str_replace(
+            ['á', 'à', 'â', 'ã', 'ä', 'é', 'è', 'ê', 'ë', 'í', 'ì', 'î', 'ï', 'ó', 'ò', 'ô', 'õ', 'ö', 'ú', 'ù', 'û', 'ü', 'ç'],
+            ['a', 'a', 'a', 'a', 'a', 'e', 'e', 'e', 'e', 'i', 'i', 'i', 'i', 'o', 'o', 'o', 'o', 'o', 'u', 'u', 'u', 'u', 'c'],
+            $data['nome_func']
+        );
+
+        // Transformar em maiúsculo
+        $data['nome_func'] = strtoupper($data['nome_func']);
+
+        //dd($data);
+        $post = Post::create($data); // Criar um novo registro na tabela "funcionarios"
+
+        //return 'Cadastro Realizado com Sucesso!';
+        return redirect()->route('posts.index');
+    }
+
 }
